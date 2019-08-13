a4cea2f3e0ba4393aa1ef7d6a92077728cf766f6
@danjharrington <danjpharrington@gmail.com>
2019-08-13 18:36:57 +0100

    New season predictions

--- pages/2020-football-predictions.vue
+++ pages/2020-football-predictions.vue
@@ -1,1 +1,36 @@
+<template>
+  <v-layout>
+    <v-flex >
+      
+      <h1>2020 Football Season Predictions</h1>
+      <hr>
+      <div class="post">
+      <p>More content to follow</p>
+</div>
 
+
+    </v-flex>
+  </v-layout>
+</template>
+
+
+<script>
+export default {
+  head () {
+    return {
+      title: 'Predictions for the 2020 football season',
+      meta: [
+        { hid: 'description', name: 'description', content: 'Opinions and predictions for the new football season' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+hr {margin:20px;}
+.post {margin-left:1pc;margin-right:1pc}
+p {
+font-size:18px;
+}
+</style>
