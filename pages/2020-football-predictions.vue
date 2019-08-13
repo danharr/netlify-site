f1206ca6de893d5531cf42a02e7de4abf48a10e3
@danjharrington <danjpharrington@gmail.com>
2019-08-13 21:43:42 +0100

    Added update to football post

--- pages/2020-football-predictions.vue
+++ pages/2020-football-predictions.vue
@@ -5,7 +5,26 @@
       <h1>2020 Football Season Predictions</h1>
       <hr>
       <div class="post">
-      <p>More content to follow</p>
+      
+<p>The Premier League kicked off again on Friday and the usual teams picked up 3 points with Man City thumping West Ham at the London Stadium and Liverpool destroying Norwich at Anfield.</p>
+
+<p>The title race will surely be between Man City and Liverpool again with both teams nearly getting 100 points in 2018-19 season.</p>
+
+<p>With Chelsea's transfer ban and the strain of Champions League games I can only see them finishing 5th or 6th. The loss of David Luiz is only going to benefit them though as I've rarely seen him out in a good performance.</p>
+
+<p>Spurs can never be dismissed and with a few new signings I can see them battling with Arsenal for 3rd place.</p>
+
+<p>Man Utd started the season brightly with a 4 nil win over Frank Lampard's Chelsea but I think they'll only get 4th spot if Arsenal have another terrible end of season run like last year.</p>
+
+<p>Everton, West Ham, Leicester and Wolves will yoyo back and forth in the middle of the pack. I can see Southampton doing better this year and maybe push one of those other 4 out and sneak a Europa League spot.</p>
+
+<p>Villa made a good signing in Heaton from Burnley which makes me think they'll stay up.</p>
+
+<p>The other promoted teams will slide back down to the Championship and could be joined by Brighton.  Brighton beat Watford on the opening day but I feel their manager won't be able to keep the momentum going beyond their inevitable annual home win against West Ham in their first home game.</p>
+
+<p>Top scorer sound be either Aubameyang, Sterling or Kane. Kane has the least competition for places and won't be rested much so provided he stays fit I reckon he'll score most.</p>
+
+
 </div>
 
 
