b361fd550646550540515095e8ba4acc79c0b324
dharring <dan.harrington@sony.com>
2019-08-23 16:33:39 +0100

    lunch time posts

--- pages/fantasy-football-standings.vue
+++ pages/fantasy-football-standings.vue
@@ -3,10 +3,10 @@
     <v-flex >
       
       <h1>Fantasy Football Predictions</h1>
-      <hr>
+      
       <div class="post">
       
-      <h2>EPL Finishing Positions</h2>
+     
           <v-chip
       class="ma-2"
       color="green"
@@ -25,12 +25,18 @@
       
       Wrong
     </v-chip>
+     <h2>EPL Finishing Positions</h2>
           <v-radio-group row v-model="finishing" >
-      <v-radio label="Winners" value="1"></v-radio>
-      <v-radio label="Second Place" value="2"></v-radio>
-      <v-radio label="Third Place" value="3"></v-radio>
-      <v-radio label="Fourth Place" value="4"></v-radio>
-      <v-radio label="Fifth Place" value="5"></v-radio>
+      <v-radio label="1st" value="1"></v-radio>
+      <v-radio label="2nd" value="2"></v-radio>
+      <v-radio label="3rd" value="3"></v-radio>
+      <v-radio label="4th" value="4"></v-radio>
+      <v-radio label="5th" value="5"></v-radio>
+      <v-radio label="18th" value="6"></v-radio>
+      <v-radio label="19th" value="7"></v-radio>
+      <v-radio label="20th" value="8"></v-radio>
+
+
     </v-radio-group>
 <table>
   <tr>
@@ -55,7 +61,34 @@
     </v-chip></td>
   </tr>
   </table>
+<hr>
+<h2>Top Goalscorer</h2>
 
+<table>
+  <tr>
+    <!-- <th style="width:20px;"> </th>
+    <th>Pos</th> -->
+    <th>Player</th>
+    <th>Goals</th>
+    <th>Predictions</th>
+  </tr>
+  <tr v-for="(x,i) in top_scorer.slice(0,10)" :key="x.player.name">
+    <!-- <td     style="text-align: center;"><img :src="x.crest" :alt="x.team" style="height:20px;margin:auto;"></td>
+    <td>{{x.pos}}</td> -->
+    <td>{{x.player}}</td>
+    <td>{{x.goals}}</td>
+        <td>
+    <v-chip
+      class="ma-2"
+      :color="i==0 ? 'green' : 'red'" v-for="p in x.pred" :key="p"
+      label
+      text-color="white"
+    >
+      
+      {{p}}
+    </v-chip></td>
+  </tr>
+  </table>
 </div>
 
 
@@ -78,9 +111,41 @@ export default {
   },
   data () {
     return {
+      top_scorer: [{ 'player': { 'id': 24121, 'name': 'Teemu Pukki', 'firstName': 'Teemu', 'lastName': null, 'dateOfBirth': '1990-03-29', 'countryOfBirth': 'Finland', 'nationality': 'Finland', 'position': 'Attacker', 'shirtNumber': 22, 'lastUpdated': '2019-08-22T03:15:37Z' }, 'team': { 'id': 68, 'name': 'Norwich City FC' }, 'numberOfGoals': 4 }],
       finishing: '1',
       runners_up: [{ 'team': 'Sheffield United FC', 'pos': 8, 'crest': 'https://upload.wikimedia.org/wikipedia/en/9/9c/Sheffield_United_FC_logo.svg' }],
       winners: [{ 'team': 'Sheffield United FC', 'pos': 8, 'crest': 'https://upload.wikimedia.org/wikipedia/en/9/9c/Sheffield_United_FC_logo.svg' }],
+      scorer: {
+        'Harry Kane': ['Al Hapgood',
+          'Alex Czopowyj',
+          'Andy Emery ',
+          'Andy Harrington ',
+          'Brunel Woods',
+          'Dan Harrington',
+          'Jon Carolan',
+          'Mark Millen',
+          'Russ Coombes',
+          'Steve Ambrose'],
+        'Mohamed Salah': ['Chris Ambrose',
+          'Keir Novesky',
+          'Lauren Smith',
+          'Matt Millen',
+          'Paul Simmons',
+          'Ping Chen'],
+        'Pierre-Emerick Aubameyang': ['Dan Mahony',
+          'Dave Groves',
+          'Jason Dawkins',
+          'Kieran Mackelden',
+          'Sam Crowhurst',
+          'Shayan Sichani'],
+        'Raheem Sterling': ['Dylan Unger',
+          'Samuel Dawkins'],
+        'Sergio Aguero': ['Dennis Ong',
+          'Joe Sutton',
+          'Luke Randall',
+          'Tony Smith',
+          'Tyler Logiudice']
+      },
       preds: {
         'Manchester City FC': ['Al Hapgood',
           'Joe Sutton',
@@ -199,6 +264,15 @@ export default {
         this.winners = y
         this.runners_up = y2
         this.third = y3
+      })
+
+    axios
+      .get('https://api.football-data.org/v2/competitions/PL/scorers', { headers: { 'X-Auth-Token': '1a69e76a83a148e8bf152ab165cf44f6' } })
+      .then(response => {
+        var y = []
+        var t = this.scorer
+        response.data.scorers.forEach(function (d) { y.push({ player: d.player.name, goals: d.numberOfGoals, pred: t[d.player.name] }) })
+        this.top_scorer = y
       })
   },
   computed: {
