99fa85d2eea65f18f9d49e9c6e88f55fb51fad58
dharring <dan.harrington@sony.com>
2019-09-04 14:57:30 +0100

    new layout

---
+++ components/allvids.vue
@@ -0,0 +1,147 @@
+<template>
+  
+<div style="margin:10px;">
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    <div style="color:black;" class="text-xs-center">
+      <v-pagination
+        v-model="page"
+        :length="Math.ceil(hacks.length/max)"
+        :total-visible="10"
+        light
+      ></v-pagination>
+      </div>
+
+
+  <div id="gallery">
+
+
+  
+
+
+
+
+  <v-card width=250 light class="videos" v-for="(vid,i) in hacks.slice((page-1)*max,max*page)" :key="i">
+        <v-img style="cursor:pointer" @click="showVid(vid.id,vid.name,vid.desc)"
+          class="white--text"
+        
+          src="https://sourcey.com/images/logos/google-drive-704x344.png"
+        >
+    
+        </v-img>
+        <v-card-title>
+                
+          <div style="height:150px;overflow:hidden;" >
+               <span  class="blue--text">{{vid.lead}}</span><br>
+            <span  class="grey--text">{{vid.name.split('_').join(' ')}}</span><br>
+           
+            <span>{{vid.desc}}</span>
+          </div>
+        </v-card-title>
+        <v-card-actions>
+          <v-btn @click="showVid(vid.id,vid.name,vid.desc)" flat color="orange">View</v-btn>
+     
+        </v-card-actions>
+      </v-card>
+    
+
+ <v-dialog
+        v-model="dialog"
+        max-width="600"
+      >
+        <v-card>
+          <v-card-title class="headline">{{name}}</v-card-title>
+  
+    
+  
+  <iframe v-if="dialog" allowfullscreen :src="'https://drive.google.com/file/d/'+id+'/preview'" width="420" height="340"></iframe>
+        <v-card-text>{{desc}}
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+
+    
+ </div>
+
+
+</div>
+</template>
+
+<script>
+/* eslint-disable*/
+
+export default {
+  name: 'allvids',
+  props:['hacks'],
+  data() {
+    return {
+      dialog:false,
+      page:1,
+      max:10,
+      id:'',
+      name:'',
+      desc:''
+    };
+  },
+  methods:{
+showVid(g,h,i){
+
+this.dialog=true
+this.id = g;
+this.name=h;
+this.desc=i;
+}
+
+    
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#gallery{
+
+    display:flex;
+flex-wrap:wrap;
+
+}
+
+
+.videos{
+margin:5px;
+
+
+ 
+}
+
+
+iframe{
+display:block;
+    border-style: none;
+    margin:auto;
+    opacity:1;
+}
+
+ h3 {
+
+    margin:10px;
+    font-family:'Share Tech', sans-serif;
+}
+
+p {
+margin:10px;
+
+}
+</style>
--- pages/video-finder.vue
+++ pages/video-finder.vue
@@ -1,60 +1,305 @@
 <template>
-  <v-layout>
-    <v-flex >
-      
-      <h1>New blog</h1>
-      <hr>
-      <div class="post">
-      <p>I've had many blogs/websites over the years.  The first were on Google's blogger covering what movies I liked.  Then I moved onto Wordpress and had some success with tips-for-excel.com.
-        At it's height I had 2,000 unique visitors a month coming to get help with vlookups and sumif() formulas.</p>
 
 
-        <p>More recently I launched table-plans.com.  This site seems an odd choice for me but it enabled me to use AWS a fair bit.  S3, Lambda and DynamoDB are the backbone of that website.
-        </p>
 
-        <p>I've been using github pages for free hosting for a few years without ever really giving it my full attention.  I initally used Hubpress to generate a static website. 
-           I then learnt about <a href="https://gohugo.io/">Hugo</a> to make static websites that you can add new posts to using simple markdown.  This site is now built using Nuxt/Vue and is server side rendered.  This means that I build the website locally and push the site the gets built (various html files) to my github repository.
-          The benefit of this approach is that the pages seen by google are full of html and content rather than just being a link to a series of javascript files.  This should lead to better SEO performance.
-          </p>
+ <v-app
+    id="inspire"
+    dark
+  >
 
-          <p>In the near future I hope to share various posts such as reviews of products I've just bought (such as the Nest Outdoor IQ camera) as well as the usual data related posts I'm often thinking of writing (I work for PlayStation in their Business Intelligence department).  Another likely topic I'll cover is how data can be used to improve searching for making puchase decisions.  I'm often amazed at how hard websites make it for users to compare phones for example.  So I'll hopefully make a few web apps that let people compare the latest dash cameras for example
-            </p>
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
-            <h3>Update</h3>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Permanent+Marker|Rubik|Share+Tech|Material+Icons' rel="stylesheet">
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      clipped
+      app
+    >
+      <v-list dense>
+        <v-subheader class="mt-3 grey--text text--darken-1">Favourites</v-subheader>
+        <v-list>
+          <v-list-tile style="cursor:pointer;" @click="showVid(item.id,item.text,item.desc)" v-for="item in items2" :key="item.text" avatar >
+            <v-list-tile-avatar>
+              <v-icon>{{item.logo}}</v-icon>
+            </v-list-tile-avatar>
+            <v-list-tile-title  v-text="item.text" ></v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+         <v-subheader class="mt-3 grey--text text--darken-1">Categories</v-subheader>
+        <v-list-tile v-for="item in items" :key="item.text" @click="office=item.code">
+          <!--<v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>-->
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ item.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        
+  
+    
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+      color="red"
+      fixed
+      clipped-left
+      app
+    >
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-icon class="mx-3">fas fa-video</v-icon>
+      <v-toolbar-title class="mr-5 align-center">
+        <span class="title">Video Finder</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-layout row align-center style="max-width: 650px">
+        <v-text-field
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          append-icon="search"
+          color="white"
+          hide-details
+                     dark
+            
+            clearable
+            v-model="search"
+           @click:append="find(search)"
+           @change="find(search)"
+         
+        ></v-text-field>
+      </v-layout>
+    </v-toolbar>
+    <v-content style="background-color:white;">
+      <v-container fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+        <div style="width:800px" class="tw"></div>
 
-            <p>I am now hosting this site on Netlify.  More details <a href="https://danharrington.netlify.com/continuous-deployment">here</a></p>
+
+
+
+
+
+
+ 
+<div v-if="none" style="display:flex;justify-content:center;flex-wrap: wrap;width: 200px;margin:auto;">
+<p>0 results found</p>
+<br>
+<v-btn small @click="reset" color="primary">Clear Search</v-btn>
+
 </div>
 
 
-    </v-flex>
-  </v-layout>
-</template>
 
+            
+
+
+  
+<allvids :hacks="videos"/>
+
+
+
+ <v-dialog
+        v-model="dialog"
+        max-width="600"
+      >
+        <v-card>
+          <v-card-title class="headline">{{name}}</v-card-title>
+  
+    
+  
+  <iframe v-if="dialog" allowfullscreen :src="'https://drive.google.com/file/d/'+id+'/preview'" width="420" height="340"></iframe>
+        <v-card-text>{{desc}}
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+
+
+
+
+
+
+
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+</v-app>
+</template>
 <script>
+/* eslint-disable*/
+import allvids from '~/components/allvids'
+import axios from 'axios'
+import Typewriter from 't-writer.js'
 
 export default {
+  name: 'App',
   layout: 'copy',
-  head () {
+  data () {
     return {
-      title: 'First post on this blog',
-      meta: [
-        { hid: 'description', name: 'description', content: 'Why I have made this blog' }
-      ]
+      dialog: false,
+      id: 'ID',
+      name: 'Name',
+      desc: 'Description',
+
+drawer: null,
+      items: [
+        { icon: 'fas fa-globe', text: 'All Categories', code: ''},
+        { icon: '', text: 'England', code: 'LO'},
+        { icon: '', text: 'Germany', code: 'DE'},
+        { icon: '', text: 'France', code: 'FR'},
+      ],
+      items2: [
+      { logo: 'fas fa-user-friends', text: 'Example Video1', id: '0B0JMGMGgxp9WMEdWb1hyQUhlOWs', desc: 'Example description'},
+      { logo: 'fas fa-piggy-bank', text: 'Example Video2', id: '0B0JMGMGgxp9WMEdWb1hyQUhlOWs', desc: 'Example description'},
+      { logo: 'fas fa-robot', text: 'Example Video3', id: '0B0JMGMGgxp9WMEdWb1hyQUhlOWs', desc: 'Example description'},
+      { logo: 'far fa-laugh-beam', text: 'Example Video4', id: '0B0JMGMGgxp9WMEdWb1hyQUhlOWs', desc: 'Example description'},
+      { logo: 'fab fa-accessible-icon', text: 'Example Video5', id: '0B0JMGMGgxp9WMEdWb1hyQUhlOWs', desc: 'Example description'},
+      { logo: 'fas fa-ghost', text: 'Example Video6', id: '0B0JMGMGgxp9WMEdWb1hyQUhlOWs', desc: 'Example description'}
+      ],
+
+      raw_videos: [],
+      office: '',
+      result: '',
+      search: null,
+      keyword: null,
+      none: false
+    }
+  },
+  components: {
+    allvids
+  },
+  methods: {
+    find (x) { this.keyword = x },
+    reset () {
+      this.keyword = ''
+      this.search = ''
+    },
+    showVid (g, h, i) {
+      this.dialog = true
+      this.id = g
+      this.name = h
+      this.desc = i
+    }
+  },
+  mounted () {
+    axios
+      .get('videos.json')
+      .then(response => {
+        console.log(response.data)
+
+        this.raw_videos = response.data
+      })
+
+    const target = document.querySelector('.tw')
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeSpeed: 80,
+      deleteSpeed: 80,
+      typeColor: 'black',
+      cursorColor: '#34ff6b'
+    })
+
+    writer
+      .type('Find videos from across the Internet. Search for topics like: Alexa')
+      .rest(500)
+      .changeOps({ deleteSpeed: 80 })
+      .remove(5)
+      .type('Football')
+      .rest(500)
+      .remove(8)
+      .type('Shopping')
+      .rest(500)
+      .remove(8)
+      .type('Politics')
+      .rest(20000)
+      .changeOps({ deleteSpeed: 1 })
+      .clear()
+      .start()
+  },
+
+  computed: {
+
+    winner () {
+      return this.raw_videos.filter(item => {
+        return item.winner === true
+      })
+    },
+    videos1 () {
+      var y = this.office
+      var z = this.result
+
+      if (y === null) { y = '' }
+      var city = this.raw_videos.filter(item => {
+        return item.name.startsWith(y)
+      })
+
+      if (z === '') { return city }
+      if (z === null) {
+        return city
+      } else {
+        return city.filter(item => {
+          return item.result === z
+        })
+      }
+    },
+    videos () {
+      var k = this.keyword
+
+      if (k === null) { k = '' }
+      k = k.toUpperCase()
+
+      var final = this.videos1.filter(item => {
+        if (item.desc.toUpperCase().includes(k) || item.name.toUpperCase().includes(k) || item.lead.toUpperCase().includes(k)) { return item }
+      })
+
+      if (final.length === 0) { this.none = true } else { this.none = false }
+
+      return final
     }
   }
 }
 </script>
-
 <style>
-hr {margin:20px;}
 
 
-.post {margin-left:1pc;margin-right:1pc}
+#title{
+
+  font-family:Rubik, arial;
+  font-size:55px;
+  text-align: center;
+  margin:0px;
+
+}
+
+#sub{
+
+  font-family:Rubik, arial;
+  font-size:20px;
+  text-align: center;
+  
+
+}
+
+iframe{
+display:block;
+    border-style: none;
+    margin:auto;
+    opacity:1;
+}
+
+ h3 {
+
+    margin:10px;
+    font-family:'Share Tech', sans-serif;
+}
 
 
-p {
+.tw {
 
-font-size:18px;
-
+  font-size:22px;
+    font-family:Rubik, arial;
 }
 </style>
